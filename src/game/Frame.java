f92a4a8aead3de5fd3d9cfcd0fb2d985f2f89464
Draesia <ramsden.ryan@live.com>
2013-09-05 17:03:05 +0100

    1 Small step for me, 1 giant leap for george

---
+++ src/game/Frame.java
@@ -0,0 +1,39 @@
+package game;
+
+import javax.swing.JFrame;
+
+public class Frame {
+
+
+	public static JFrame sframe;
+	public static int maxX = 1440;
+	public static int maxY = 720;
+	public static void main(String[] args)
+	{
+		sframe = new JFrame("RogueLike");
+		sframe.setSize(maxX, maxY);
+		sframe.add(new Game());
+		sframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sframe.setVisible(true);
+		sframe.setResizable(true);
+	}
+	public static void start()
+	{
+		sframe = new JFrame("RogueLike");
+		sframe.setSize(maxX, maxY);
+		sframe.add(new Game());
+		sframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sframe.setVisible(true);
+		sframe.setResizable(true);
+	}
+	public static int maxX()
+	{
+		return sframe.getWidth();
+	}
+	public static int maxY()
+	{
+		return sframe.getHeight();
+	}
+
+
+}
---
+++ src/game/Game.java
@@ -0,0 +1,67 @@
+package game;
+
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JPanel;
+
+
+public class Game extends JPanel implements ActionListener{
+
+	public static boolean[] mouseDown = new boolean[4];
+	public static Image[] images = new Image[30];
+	public static int cx, cy, sx, sy;
+	
+	public Game()
+	{
+		addKeyListener(new AL());
+		addMouseListener(new MouseAdapter()
+		{
+			@Override
+			public void mousePressed(MouseEvent mEvt) {
+				sx = mEvt.getX();
+				sy = mEvt.getY();
+			}
+
+		});
+		addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent evt) {
+				if(evt.getModifiers() != 16) return;
+				int maxCenterX = Tile.SIZE*Level.sizeX - Frame.maxX() + 32;
+				int maxCenterY = Tile.SIZE*Level.sizeY - Frame.maxY() + 32;
+				int differenceX = (int) (((sx) - evt.getX())*1.5);
+				int differenceY = (int) (((sy) - evt.getY())*1.5);
+				sx = evt.getX();
+				sy = evt.getY();
+
+				if(cx+differenceX < 0) 
+					cx = 0;
+				else if(cx+differenceX > maxCenterX)
+					cx = maxCenterX;
+				else 
+					cx = cx + differenceX;
+
+				if(cy+differenceY < 0)
+					cy = 0;
+				else if(cy+differenceY > maxCenterY) 
+					cy = maxCenterY;
+				else
+					cy = cy + differenceY;
+			}
+		});
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	private class AL extends KeyAdapter
+	{
+	}
+}
